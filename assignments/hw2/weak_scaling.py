bdbc7a4afb5d76713a65156c48dbef2724e7a635
Shahin John J S <ivin-98@tamu.edu>
2025-02-13 19:24:32 -0600

    updated thread counts and fixed execution file

--- assignments/hw2/weak_scaling.py
+++ assignments/hw2/weak_scaling.py
@@ -3,23 +3,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 executables_list = [
-    {"Impl": "mpi_reduce_logp", "Std": "mpi_reduce_logp_std"},
-    {"Impl": "mpi_gather_logp", "Std": "mpi_gather_logp_std"},
-    {"Impl O(P)": "mpi_broadcast_p", "Std": "mpi_broadcast_logp_std", "Imple O(log(P))": "mpi_broadcast_logp"},
-    {"Impl": "mpi_all_gather_p", "Std": "mpi_all_gather_p_std"},
+    {"Impl reduce": "mpi_reduce_logp", "Std reduce": "mpi_reduce_logp_std"},
+    {"Impl gather": "mpi_gather_logp", "Std gather": "mpi_gather_logp_std"},
+    {"Impl broadcast O(P)": "mpi_broadcast_p", "Std broadcast": "mpi_broadcast_logp_std", "Impl broadcast O(log(P))": "mpi_broadcast_logp"},
+    {"Impl all gather": "mpi_all_gather_p", "Std all gather": "mpi_all_gather_p_std"},
 ]
 
 for executables in executables_list:
 
-    thread_counts = [1, 2, 3, 4]
+    thread_counts = [1, 2, 3, 4, 5, 6, 7, 8]
     # element_counts = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200]
-    element_counts = [1, 2, 3, 4]
-
-    execution_times = {key: [] for key in executables.values()}
+    element_counts = [1, 2, 3, 4, 5, 6, 7, 8]
+    
+    execution_times = {key: [] for key in executables.keys()}
     for label, exe in executables.items():
         for P, E in zip(thread_counts, element_counts):
             result = subprocess.run(
-                ["mpirun", "-np", str(P), {exe}],
+                ["mpirun", "-np", str(P), exe],
                 capture_output=True, text=True
             )
             for line in result.stdout.splitlines():
