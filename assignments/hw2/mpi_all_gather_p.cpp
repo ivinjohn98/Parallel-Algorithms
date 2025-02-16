eeb05d5f71b654446809ba50b4fb0e1cf65150b1
Shahin John J S <ivin-98@tamu.edu>
2025-02-16 16:52:38 -0600

    Modified implementation

--- assignments/hw2/mpi_all_gather_p.cpp
+++ assignments/hw2/mpi_all_gather_p.cpp
@@ -18,23 +18,30 @@ int main(int argc, char **argv)
   int data = rank;
   std::vector<int> data_gattered;
 
+  MPI_Barrier(MPI_COMM_WORLD);
+  double start_time = MPI_Wtime(); // start time
+  data_gattered.push_back(data);
+
   int nextP = (rank + 1) % world_size;
   int prevP = (rank - 1 + world_size) % world_size;
 
   int next_data_to_send = data;
   int recv_buffer;
 
-  MPI_Barrier(MPI_COMM_WORLD);
-  double start_time = MPI_Wtime(); // start time
-  data_gattered.push_back(data);
-
   for (size_t i = 0; i < world_size - 1; ++i)
   { // P-1 iterations
-    MPI_Send(&next_data_to_send, 1, MPI_INT, nextP, 0, MPI_COMM_WORLD);
-    MPI_Recv(&recv_buffer, 1, MPI_INT, prevP, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    MPI_Request requests[2];
+    MPI_Status status[2];
+    
+    MPI_Irecv(&recv_buffer, 1, MPI_INT, prevP, 0, MPI_COMM_WORLD, &requests[0]);
+    MPI_Isend(&next_data_to_send, 1, MPI_INT, nextP, 0, MPI_COMM_WORLD, &requests[1]);
+    MPI_Waitall(2, requests, status);
+
     data_gattered.push_back(recv_buffer);
     next_data_to_send = recv_buffer;
   }
+
   MPI_Barrier(MPI_COMM_WORLD);
   double end_time = MPI_Wtime(); // end time
 
