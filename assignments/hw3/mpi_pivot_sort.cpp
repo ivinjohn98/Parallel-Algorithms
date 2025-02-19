659941e957574042857988ddf3da987a8a0aee46
Shahin John J S <ivin-98@tamu.edu>
2025-02-18 18:06:45 -0600

    Added already existing mpi pivot sort

---
+++ assignments/hw3/mpi_pivot_sort.cpp
@@ -0,0 +1,167 @@
+#include <iostream>
+#include <mpi.h>
+#include <random>
+#include <vector>
+#include <algorithm>
+
+int world_rank = -1;
+int world_size = -1;
+void print_in_order(const std::vector<int>& local_data) {
+  // Assume local_data.size() could be different on each rank.
+  int ps_send = local_data.size();
+  int ps_recv = 0;
+
+  // MPI provided function for Prefix Sum.
+  MPI_Exscan(&ps_send, &ps_recv, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+  //std::cout << "PS: " << world_rank << " " << ps_recv << std::endl;
+
+  for(int token = 0;  token < world_size; ++token) {
+    if(token == world_rank){
+      for(size_t i = 0; i < local_data.size(); ++i) {
+        std::cout << world_rank << ": index " << ps_recv + i 
+                  << " = " << local_data[i] << std::endl;
+      }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);   // O(lg(p))
+  }
+}
+
+
+
+int main(int argc, char **argv) {
+  // Initialize MPI
+  MPI_Init(&argc, &argv);
+
+  // Get the rank of the process
+ 
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+  // Get the total number of ranks
+
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  std::cout << "Howdy from " << world_rank << " of " << world_size << std::endl;
+
+
+
+  std::mt19937 gen(world_rank); 
+  std::uniform_int_distribution<> distrib(0, 100);
+  
+  std::vector<int> local_data;
+  for(int i = 0; i<5; ++i) {
+    local_data.push_back(distrib(gen));
+  }
+
+
+  print_in_order(local_data);
+
+
+  //
+  // Sample P pivots   O(1)
+  // TODO Homework 3 -- Sample k pivots per process
+  std::uniform_int_distribution<> sampler(0, local_data.size()-1);
+  int local_sampled_pivot = local_data[sampler(gen)];
+
+  std::vector<int> pivots(world_size, 0);
+  
+  //
+  // Sharing of pivots O(P)
+  MPI_Allgather(&local_sampled_pivot, 1, MPI_INT,
+    pivots.data(), 1, MPI_INT, MPI_COMM_WORLD);
+
+  //
+  // Assume Quicksort:   O(P*lg(P))
+  std::sort(pivots.begin(), pivots.end());
+
+  //
+  // TODO Homework 3 -- Pick "best" P-1 pivots from oversampled
+  pivots.pop_back();
+
+  if(world_rank == 2) {
+    std::cout << "PIVOTS = ";
+    for(int p : pivots) {
+      std::cout << p << " ";
+    }
+    std::cout << std::endl;
+  }
+
+  std::vector< std::vector<int> > send_bufs(world_size);
+  
+  //
+  // Creating send buckets:  O(N/P * lg(P))
+  for(int val : local_data) {
+    auto lbi = std::lower_bound(pivots.begin(), pivots.end(), val);
+    int dest_rank = std::distance(pivots.begin(), lbi);
+
+    send_bufs.at(dest_rank).push_back(val);
+  }
+
+  if(world_rank == 0) {
+    for(int dr = 0; dr < world_size; ++dr) {
+      std::cout << "Sending to " << dr << ": ";
+      for(int val : send_bufs[dr]) {
+        std::cout << val << " ";
+      }
+      std::cout << std::endl;
+    }
+  }
+
+  std::vector< std::vector<int> > recv_bufs(world_size);
+  std::vector< int > size_recv(world_size, 0);
+  std::vector< MPI_Request > size_recv_req(world_size);
+  // Sets up P MPI_Irecvs to get future send size O(P)
+  for(int i = 0; i < world_size; ++i) {
+    MPI_Irecv(&size_recv[i], 1, MPI_INT, i,
+              0, MPI_COMM_WORLD, &size_recv_req[i]);
+  }
+  // Send future send size to all ranks O(P)
+  for(int i = 0; i < world_size; ++i) {
+    int to_rank = (world_rank + i) % world_size;
+    int to_send = send_bufs[to_rank].size();
+    MPI_Send(&to_send, 1, MPI_INT, to_rank, 0, MPI_COMM_WORLD);
+  }
+  MPI_Waitall(size_recv_req.size(), size_recv_req.data(), 
+    MPI_STATUS_IGNORE);
+
+  // Post Irecv for every sending rank O(P)
+  std::vector< MPI_Request > data_recv_req(world_size);
+  for(int i = 0; i < world_size; ++i) {
+    recv_bufs.at(i).resize(size_recv.at(i));
+    MPI_Irecv(recv_bufs.at(i).data(), size_recv.at(i), MPI_INT, i,
+              0, MPI_COMM_WORLD, &data_recv_req[i]);
+  }
+  // Send data to every rank O(P)
+  for(int i = 0; i < world_size; ++i) {
+    int to_rank = (world_rank + i) % world_size;
+    MPI_Send(send_bufs[to_rank].data(), send_bufs[to_rank].size(), 
+             MPI_INT, to_rank, 0, MPI_COMM_WORLD);
+  }
+  MPI_Waitall(data_recv_req.size(), data_recv_req.data(), 
+    MPI_STATUS_IGNORE);
+
+
+  //  If balanced:   O(N/P)
+  std::vector<int> sorted_data;
+  for(int i = 0; i < world_size; ++i) {
+    for(int val : recv_bufs[i]){
+      sorted_data.push_back(val);
+    }
+  }
+
+  // If baalanced:   O(N/P * lg(N/P))
+  MPI_Barrier(MPI_COMM_WORLD);
+  double start_time = MPI_Wtime();
+  std::sort(sorted_data.begin(), sorted_data.end());
+  MPI_Barrier(MPI_COMM_WORLD);
+  double end_time = MPI_Wtime();
+
+
+  //OVERALL SO FAR, if N>>P & data is balanced:   O(N/P * lg(N/P))
+
+  //TODO Homework 3 -- balance sorted_data vector
+
+  print_in_order(sorted_data);
+  // Finalize MPI
+  MPI_Finalize();
+}
